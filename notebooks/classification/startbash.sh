38f0fd7858b6c652e47d44b40080f2dd3b778fee
dbdimitrov <d.boykoff@gmail.com>
2023-06-12 16:05:45 +0200

    extend runtime

--- notebooks/classification/startbash.sh
+++ notebooks/classification/startbash.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=100000
 #SBATCH --cpus-per-task 2
-#SBATCH --time=5:30:00
+#SBATCH --time=11:30:00
 #SBATCH --job-name="liana2"
 #SBATCH --output=liana2.out
 #SBATCH --mail-user=daniel.dimitrov@uni-heidelberg.de

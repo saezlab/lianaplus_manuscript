b573660c8ee60c904c10d689a1079cd985064f83
dbdimitrov <d.boykoff@gmail.com>
2023-06-17 12:52:26 +0200

    add bash

---
+++ notebooks/spot_calling/startbash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p single
+#SBATCH -N 1
+#SBATCH --mem=100000
+#SBATCH --cpus-per-task 48
+#SBATCH --time=48:00:00
+#SBATCH --job-name="liana2"
+#SBATCH --output=liana2.out
+#SBATCH --mail-user=daniel.dimitrov@uni-heidelberg.de
+#SBATCH --mail-type=ALL
+#SBATCH --requeue
+#SBATCH --chdir /net/data.isilon/ag-saez/bq_ddimitrov/Repos/liana2_manuscript/notebooks/spot_calling
+
+python $1 $2

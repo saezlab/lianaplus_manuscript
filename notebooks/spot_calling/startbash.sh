14e0bbb1b462c042c7a744046b71c649bc954554
dbdimitrov <d.boykoff@gmail.com>
2023-06-17 13:06:17 +0200

    reduce number of cpus

--- notebooks/spot_calling/startbash.sh
+++ notebooks/spot_calling/startbash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -p single
 #SBATCH -N 1
-#SBATCH --mem=100000
-#SBATCH --cpus-per-task 48
+#SBATCH --mem=50000
+#SBATCH --cpus-per-task 8
 #SBATCH --time=48:00:00
 #SBATCH --job-name="liana2"
-#SBATCH --output=liana2.out
+#SBATCH --output=spot_calling.out
 #SBATCH --mail-user=daniel.dimitrov@uni-heidelberg.de
 #SBATCH --mail-type=ALL
 #SBATCH --requeue
